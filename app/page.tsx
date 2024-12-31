c30bfc03c9a8684daa96e4c70db617ecb25b62ab
abel <abeloabate01@gmail.com>
2024-12-31 15:51:35 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,16 @@ const YourComponent = () => {
     document.body.appendChild(script);
 
     script.onload = () => {
-      console.log("Telegram SDK loaded");
+
       const Telegram = window.Telegram;
       if (Telegram?.WebApp) {
         Telegram.WebApp.ready();
-        console.log("Telegram WebApp is ready");
 
         setTimeout(() => {
-          console.log("Closing current Mini App and redirecting...");
+
           Telegram.WebApp.close(); // Closes the current Mini App
           window.location.href = "https://t.me/PaxyoMini_bot?startapp";
-        }, 10);
+        }, 1);
       } else {
         console.error("Failed to initialize Telegram WebApp.");
       }
@@ -31,10 +30,8 @@ const YourComponent = () => {
   }, []);
 
   return (
-    <div>
-      <h1>  Mini App</h1>
-
-    </div>
+    <>
+    </>
   );
 };
 
