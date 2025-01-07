49dcc95290b34c1b00e69c45e24161db2770eb8d
abel <abeloabate01@gmail.com>
2025-01-07 12:27:28 +0300

    Bb

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,13 @@ const YourComponent = () => {
   useEffect(() => {
     // Ensure code runs only on the client side
     if (typeof window !== "undefined") {
-      try {
-        // Attempt to open the link
-        WebApp.openTelegramLink("https://t.me/PaxyoMini_bot?startapp=miniapp");
+      // Close the current Mini App
+      WebApp.close();
 
-        // If no errors, close the Mini App after a short delay
-        setTimeout(() => {
-          WebApp.close();
-        }, 100); // Adjust delay as needed
-      } catch (error) {
-        console.error("Failed to redirect using WebApp.openLink:", error);
-        // Handle error (optional: show a message or retry logic)
-      }
+      // Redirect to another Telegram Mini App
+      window.location.href = "https://t.me/PaxyoMini_bot?startapp=miniapp";
     }
-  }, []); // Empty dependency array ensures it runs once when the component mounts
-  // Empty dependency array ensures it runs once when component mounts
+  }, []); // Empty dependency array ensures it runs once when component mounts
 
   return (
     <>
