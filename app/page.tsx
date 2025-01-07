a361af6f4581ff157f2a79ea0c4559276b82e2a4
abel <abeloabate01@gmail.com>
2025-01-07 12:11:30 +0300

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,25 @@
 import { useEffect } from "react";
 import WebApp from "@twa-dev/sdk";
 
-useEffect(() => {
-  // Ensure code runs only on the client side
-  if (typeof window !== "undefined") {
-    // Close the current Mini App
-    WebApp.close();
+const YourComponent = () => {
+  useEffect(() => {
+    // Ensure code runs only on the client side
+    if (typeof window !== "undefined") {
+      // Close the current Mini App
+      WebApp.close();
 
-    // Redirect to another Telegram Mini App after closing
-    setTimeout(() => {
-      window.location.href = "https://t.me/PaxyoMini_bot?startapp";
-    }, 100); // 100ms delay to allow WebApp.close() to complete
-  }
-}, []); // Empty dependency array ensures it runs once when component mounts
+      // Redirect to another Telegram Mini App after closing
+      setTimeout(() => {
+        window.location.href = "https://t.me/PaxyoMini_bot?startapp";
+      }, 100); // 100ms delay to allow WebApp.close() to complete
+    }
+  }, []); /// Empty dependency array ensures it runs once when component mounts
 
-return (
-  <>
-    {/* Optionally, render some content */}
-  </>
-);
+  return (
+    <>
+      {/* Optionally, render some content */}
+    </>
+  );
 };
 
 export default YourComponent;
